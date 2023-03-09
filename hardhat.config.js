73c0af19e082253c97fadd2dc369e3b2bc5901a6
Eli Druzh <ilya.druzh@gmail.com>
2023-03-09 20:23:23 +0300

    add some info for compile

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const privateKey = process.env.PRIVATE_KEY
-const endpoint = process.env.URL
+const privateKey = process.env.PRIVATE_KEY !== undefined ? process.env.PRIVATE_KEY : 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+const endpoint = process.env.URL !== undefined ? process.env.URL : "http://127.0.0.1:8545"
 
 module.exports = {
   solidity: '0.8.18',
