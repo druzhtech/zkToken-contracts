1ef5c45642e58b97cec5311fdf4a77393ca4b256
Sergey <68071039+mysteryon88@users.noreply.github.com>
2023-04-06 17:36:58 +0300

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,7 @@ const privateKey =
     : 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 const endpoint =
   process.env.URL !== undefined ? process.env.URL : 'http://127.0.0.1:8545'
+const etherscanKey = process.env.ETHERSCAN_KEY
 
 module.exports = {
   solidity: '0.8.18',
@@ -16,6 +17,11 @@ module.exports = {
     sepolia: {
       url: endpoint,
       accounts: [`0x${privateKey}`],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: etherscanKey,
     },
   },
 }
